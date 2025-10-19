385c333728cd3ed89452f25ed2161cbf03c60f68
Sarvesh Neelraj <sarveshneel@gmail.com>
2025-10-19 09:56:17 +0400

    i have to learn js D:

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
 let correctAnswers = 0;
+let currentQuestion = 1;
+const myDiv = document.getElementById('question');
 function checkAnswer1() {
     
     const selected = document.querySelector('input[name="q1"]:checked');
@@ -20,3 +22,18 @@ function checkAnswer1() {
   if (navEntries.length > 0 && navEntries[0].type === "reload") {
     window.location.href = "index.html";
   }
+
+if (currentQuestion === 1) {
+ myDiv.innerHTML = `<label>Who is the main character of Silksong?</label><br>
+                    <input type="radio" name="q1" value="a"> Hornet<br>
+                    <input type="radio" name="q1" value="b"> The Knight<br>
+                    <input type="radio" name="q1" value="c"> Zote<br>
+                    <button type="button" onclick="checkAnswer1()" id="submit-q1">Submit Answer</button>
+                    <button type="button" onclick="nextQuestion(1)" id="next-q1">Next Question</button>
+ `;
+	} else if (currentQuestion === 2) {
+ myDiv.innerHTML = `
+ `;
+	} else {
+ 
+ myDiv.innerHTML = ``}
